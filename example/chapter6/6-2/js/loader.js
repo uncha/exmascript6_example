bb6db1f07f48d674e9b53d1e908056f4d173cdb4
kyutaekim <kyutae21c@naver.com>
2017-09-14 18:36:55 +0900

    chapter 6-2

---
+++ example/chapter6/6-2/js/loader.js
@@ -0,0 +1,22 @@
+class Loader{
+	constuctor(){
+
+	}
+
+	load({url, callback}){
+		const promise = new Promise((resolve, reject)=>{
+			let req = new XMLHttpRequest();
+			req.open('GET', url);
+			req.send();
+			req.onreadystatechange = ()=>{
+			    if(req.readyState == req.DONE){
+			        if(req.status == 200){
+			            callback.call(this, req.response);
+			        } else {
+			            console.log(req.statusText);
+			        }
+			    }
+			}
+		});
+	}
+}
